3ce5474ccdc17d87171f4b60b3bca5e64ab53326
MartinCalamel <martin.calamel@cpe.fr>
2024-12-31 11:47:33 +0100

    algo de determinant

--- code/matrice.py
+++ code/matrice.py
@@ -165,6 +165,25 @@ class Matrice:
         sub_array =[[M[i][j] for j in ind_cols] for i in ind_lines]
         return Matrice(sub_array)
     
+    def select(self, indice: int, mat: 'Matrice'):
+        """
+        Retourne la matrice sauf la ligne de l'indice et la première colone
+
+        Permet de calculer le determinant de façon recursive (development en ligne) 
+
+        #input : indice de la ligne a enlever, matrice de depart
+        #output : matrice recalculer.
+        """
+        mat_res = []
+        for i in range(mat.dim[0]):
+            if i != indice:
+                ligne = [mat.matrice[i][j] for j in range(1, mat.dim[1])]
+                mat_res.append(ligne)
+        return Matrice(mat_res)
+
+        
+
+    
     def det(self):
         """
         fonction pour calculer le determinant de la matrice self
@@ -188,7 +207,7 @@ class Matrice:
         else :
             det = 0
             for i in range(matrice.dim[0]):
-                det += matrice.matrice[i][0]*self._det(Matrice(matrice[:i,1:]+matrice[i+1:,1:]))
+                det += matrice.matrice[i][0]*self._det(self.select(i,matrice))
             return det
     def is_carre(self) -> bool:
         """
@@ -214,7 +233,7 @@ if __name__ == "__main__":
     A1 = Matrice([[1,2],[2,1]])
     B2 = Matrice([[0,1],[1,0]])
 
-    print(A1[1:1,1:1])
+    print(A.select(2,A))
 
     print(A1@B2)
 
